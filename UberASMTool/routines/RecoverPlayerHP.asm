0e755c042460f33669e81211ee326ef62bcd8bc5
? <GhbSmwc@users.noreply.github.com>
2025-08-14 20:18:13 -0400

    whoopsies
    -Forgot to apply the use of !Setting_PlayerHP_TrueMaximumHPAndDamageValue for SubtractPlayerHP.asm
    
    -Added damage and heal numbers for gradual HP changes (slow damage and slow recovery)

--- UberASMTool/routines/RecoverPlayerHP.asm
+++ UberASMTool/routines/RecoverPlayerHP.asm
@@ -13,33 +13,65 @@
 	;This is for use with the gradual heal block.
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	?RecoverPlayerHP0:
-	if !Setting_PlayerHP_TwoByte == 0
-		LDA !Freeram_PlayerHP_CurrentHP		;\Health + Recovery
-		CLC					;|
-		ADC $00					;/
-		BCC ?.NotMaxed				;>If not exceeding 255, compare with max HP
-		CMP !Freeram_PlayerHP_MaxHP		;\If not exceeding max HP, write to HP.
-		BCC ?.NotMaxed				;/
-		
-		?.Maxed
-		LDA !Freeram_PlayerHP_MaxHP
-		
-		?.NotMaxed
-		STA !Freeram_PlayerHP_CurrentHP
-	else
-		REP #$20
-		LDA !Freeram_PlayerHP_CurrentHP
-		CLC
-		ADC $00
-		BCS ?.Maxed				;>If not exceeding 65535
-		CMP !Freeram_PlayerHP_MaxHP
-		BCC ?.NotMaxed				;>If not exceeding max HP, write to HP.
-		
-		?.Maxed
-		LDA !Freeram_PlayerHP_MaxHP
-		
-		?.NotMaxed
-		STA !Freeram_PlayerHP_CurrentHP
-		SEP #$20
-	endif
+		?.DisplayRecovery
+			if !Setting_PlayerHP_DisplayRecoveryTotal
+				LDA.b #!Setting_PlayerHP_DamageHeal_Duration
+				STA !Freeram_PlayerHP_RecoveryTotalTimerDisplay
+				if !Setting_PlayerHP_TwoByte != 0
+					REP #$20
+				endif
+				LDA !Freeram_PlayerHP_RecoveryTotalDisplay
+				CLC
+				ADC $00
+				BCS ?..Overflow
+				if !Setting_PlayerHP_TwoByte != 0
+					CMP.w #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+				else
+					CMP.b #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+				endif
+				BCC ?..Write
+				
+				?..Overflow
+					if !Setting_PlayerHP_TwoByte != 0
+						LDA.w #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+					else
+						LDA.b #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+					endif
+				?..Write
+					STA !Freeram_PlayerHP_RecoveryTotalDisplay
+				?..Done
+				if !Setting_PlayerHP_TwoByte != 0
+					SEP #$20
+				endif
+			endif
+		?.RecoverHP
+			if !Setting_PlayerHP_TwoByte == 0
+				LDA !Freeram_PlayerHP_CurrentHP		;\Health + Recovery
+				CLC					;|
+				ADC $00					;/
+				BCC ?.NotMaxed				;>If not exceeding 255, compare with max HP
+				CMP !Freeram_PlayerHP_MaxHP		;\If not exceeding max HP, write to HP.
+				BCC ?.NotMaxed				;/
+				
+				?.Maxed
+				LDA !Freeram_PlayerHP_MaxHP
+				
+				?.NotMaxed
+				STA !Freeram_PlayerHP_CurrentHP
+			else
+				REP #$20
+				LDA !Freeram_PlayerHP_CurrentHP
+				CLC
+				ADC $00
+				BCS ?.Maxed				;>If not exceeding 65535
+				CMP !Freeram_PlayerHP_MaxHP
+				BCC ?.NotMaxed				;>If not exceeding max HP, write to HP.
+				
+				?.Maxed
+				LDA !Freeram_PlayerHP_MaxHP
+				
+				?.NotMaxed
+				STA !Freeram_PlayerHP_CurrentHP
+				SEP #$20
+			endif
 	RTL
--- UberASMTool/routines/SubtractPlayerHPNonRoll.asm
+++ UberASMTool/routines/SubtractPlayerHPNonRoll.asm
@@ -26,29 +26,56 @@
 	;"endif" before "if !Setting_PlayerHP_RollingHP != 0".
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	?SubtractPlayerHPNonRoll:
-	if !Setting_PlayerHP_TwoByte == 0
-		LDA !Freeram_PlayerHP_CurrentHP		;\Health - damage
-		SEC					;|
-		SBC $00					;/
-		BCS ?.NotPastZero			;>If value didn't subtract by larger value, go write HP.
-		
-		?.PastZero
-		LDA #$00				;>Otherwise set HP to 0.
-		
-		?.NotPastZero
-		STA !Freeram_PlayerHP_CurrentHP		;>Write HP value.
-	else
-		REP #$20
-		LDA !Freeram_PlayerHP_CurrentHP
-		SEC
-		SBC $00
-		BCS ?.NotPastZero
-		
-		?.PastZero
-		LDA #$0000
-		
-		?.NotPastZero
-		STA !Freeram_PlayerHP_CurrentHP
-		SEP #$20
-	endif
-	RTL
+		?.DisplayDamage
+			if !Setting_PlayerHP_DisplayDamageTotal
+				LDA.b #!Setting_PlayerHP_DamageHeal_Duration
+				STA !Freeram_PlayerHP_DamageTotalTimerDisplay
+				if !Setting_PlayerHP_TwoByte != 0
+					REP #$20
+				endif
+				LDA !Freeram_PlayerHP_DamageTotalDisplay
+				CLC
+				ADC $00
+				BCS ?..Overflow
+				if !Setting_PlayerHP_TwoByte != 0
+					CMP.w #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+				else
+					CMP.b #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+				endif
+				BCC ?..Write
+				
+				?..Overflow
+					if !Setting_PlayerHP_TwoByte != 0
+						LDA.w #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+					else
+						LDA.b #!Setting_PlayerHP_TrueMaximumHPAndDamageValue
+					endif
+				?..Write
+					STA !Freeram_PlayerHP_DamageTotalDisplay
+				?..Done
+				if !Setting_PlayerHP_TwoByte != 0
+					SEP #$20
+				endif
+			endif
+		?.SubtractHP
+			if !Setting_PlayerHP_TwoByte != 0
+				REP #$20
+			endif
+			LDA !Freeram_PlayerHP_CurrentHP		;\Health - damage
+			SEC					;|
+			SBC $00					;/
+			BCS ?.NotPastZero			;>If value didn't subtract by larger value, go write HP.
+			
+			?.PastZero
+			if !Setting_PlayerHP_TwoByte != 0
+				LDA.w #$0000
+			else
+				LDA.b #$00				;>Otherwise set HP to 0.
+			endif
+			
+			?.NotPastZero
+			STA !Freeram_PlayerHP_CurrentHP		;>Write HP value.
+			if !Setting_PlayerHP_TwoByte != 0
+				SEP #$20
+			endif
+		RTL
